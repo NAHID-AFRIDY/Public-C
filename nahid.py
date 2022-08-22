28e1da7ee1733308de3b0aa4bf07f369c3976cdc
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-22 11:57:18 +0600

    Update nahid.py

--- nahid.py
+++ nahid.py
@@ -1,4 +1,11 @@
-import os
-os.system("git pull")
-import king
-exit()
+import os, platform
+os.system('git pull')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from C23 import menu
+    menu()
+elif bit == '32bit':
+    from king import menu
+    menu()
